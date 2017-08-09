127d482989452921773f5d91ded5693150093f30
Daniel Palomino <dapalominop@gmail.com>
2017-08-09 14:37:06 -0500

    Update file sshd_config.

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 from fabistrano.deploy import *
+from fabric.api import cd
 
 username = raw_input('Servers Username:')
 password = raw_input('Servers Password:')
@@ -14,3 +15,11 @@ env.git_clone = 'git@github.com:dpalominop/InteractiveMenu.git' # Your git url
 env.restart_cmd = '' # Restart command
 # or
 # env.wsgi_path = "app_name/apache.wsgi" # Relative path to the wsgi file to be touched on restart
+
+@task
+@with_defaults
+def update_sshd_config():
+    """Update file /etc/ssh/sshd_config on all servers"""
+    with cd("%s/%s/current"%(env.base_dir, env.app_name)):
+        sudo_run("echo 'Match User *,!root\n\tForceCommand %s/%s/current/src/menu.py' >> /etc/ssh/sshd_config"%(env.base_dir, env.app_name))
+        sudo_run("service sshd restart")
